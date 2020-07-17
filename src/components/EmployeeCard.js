c752761ea95154c959a3c724da97745f777f52a0
rodriguezh21 <rodriguezh21@gmail.com>
2020-07-16 22:47:05 -0500

    added sorting function and filter

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import API from './utils/API';
+import React, { Component } from 'react'
+import API from './utils/API'
 
 
 
@@ -10,11 +10,9 @@ class EmployeeCard extends Component {
     lastName: '',
     email: '',
     picture: '',
-    gender: ''
-    
+    gender: '',
+    filteredArr: []
   }
-
-  
 
   componentDidMount () {
     this.searchName('https://randomuser.me/api/?results=200&nat=us')
@@ -23,42 +21,45 @@ class EmployeeCard extends Component {
   searchName = query => {
     API.search(query)
       .then(res => {
-        this.setState({ results: res.data.results})
+        this.setState({ results: res.data.results })
         console.log(this.state.results)
-
-        // Adding sort function
-        const sorting = this.results.sort(function (a, b) {
-          if(a.name.last < b.name.last)
-          return -1;
-          if(a.name.last > b.name.last)
-          return 1;
-          return 0;
-        })
       })
-      
+
       .catch(err => console.log(err))
   }
+// sort by name function
+  sortArr = () => {
+    const temp = this.state.results.sort(function (a, b) {
+      if (a.name.first < b.name.first) return -1
+      if (a.name.first > b.name.first) return 1
 
-  
-
-  handleInputChange = event => {
-    const name = event.target.name
-    const value = event.target.value
-    this.setState({
-      [name]: value
+      return 0
     })
+    this.setState({ ...this.state, results: temp })
+    console.log(this.state.results)
+  }
+// Could not get this to work
+// Tried to filter by gender
+  filterArr = () => {
+    const filtered = this.state.results.filter(item => item.gender);
+    console.log(filtered)
   }
 
-  // When the form is submitted, search the Giphy API for `this.state.search`
+
   handleFormSubmit = event => {
     event.preventDefault()
     this.searchName(this.state.search)
   }
-  
+
   render () {
     return (
-      <div>
-        <table striped bordered hover>
+      <div className="container">
+        
+        <button onClick={this.sortArr}type="button" className="btn btn-success">Sort by Name</button>
+        <button onClick={this.filterArr}type="button" className="btn btn-success">Filter by Gender</button>
+        
+
+        <table>
           <thead>
             <tr>
               <th>Image</th>
@@ -69,17 +70,19 @@ class EmployeeCard extends Component {
           </thead>
           {this.state.results.map(item => (
             <tbody>
-            <tr>
-              <td>
-                <img src={item.picture.medium} alt=''></img>
-              </td>
+              <tr>
+                <td>
+                  <img src={item.picture.medium} alt=''></img>
+                </td>
 
-              <td>{item.name.first} {item.name.last}</td>
+                <td>
+                  {item.name.first} {item.name.last}
+                </td>
 
-              <td>{item.email}</td>
+                <td>{item.email}</td>
 
-              <td>{item.gender}</td>
-            </tr>
+                <td>{item.gender}</td>
+              </tr>
             </tbody>
           ))}
         </table>
@@ -87,6 +90,5 @@ class EmployeeCard extends Component {
     )
   }
 }
-
 
 export default EmployeeCard
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,11 @@
 import React from "react";
 
 
-
-
-
-function Navbar(props) {
+function Navbar() {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a href="#home" onClick={() => props.handlePageChange("Home")} className={`nav-link ${props.currentPage === 'Home' && 'active'}`}>
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a href="#about" onClick={() => props.handlePageChange("About")} className={`nav-link ${props.currentPage === 'About' && 'active'}`}>
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a href="#blog" onClick={() => props.handlePageChange("Blog")} className={`nav-link ${props.currentPage === 'Blog' && 'active'}`}>
-          Blog
-        </a>
-      </li>
-      <li className="nav-item">
-        <a href="#contact" onClick={() => props.handlePageChange("Contact")} className={`nav-link ${props.currentPage === 'Contact' && 'active'}`}>
-          Contact
-        </a>
-      </li>
-    </ul>
+    <nav className="navbar navbar-dark bg-dark">
+  <span className="navbar-brand mb-0 h1">Employee Directory</span>
+</nav>
   );
 }
 
